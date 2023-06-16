b3a02713ef119cf6ca543cab0eeabb87e2c805c7
NVH <nguyenvanhung08012002@gmail.com>
2023-06-16 18:11:46 +0700

    update

--- src/components/ResetPassword/ResetPassword.js
+++ src/components/ResetPassword/ResetPassword.js
@@ -6,71 +6,36 @@ import axios from "axios";
 function Login() {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [resetCode, setResetCode] = useState("");
+  const [inputEmail, setInputEmail] = useState(false);
+  const [inputCode, setInputCode] = useState(false);
+  const [inputPassword, setInputPassword] = useState(false);
+  const [submittedEmail, setSubmittedEmail] = useState(false);
+  const [submitButtonType, setSubmitButtonType] = useState("email");
 
-  useEffect(() => {
-    const token = sessionStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true);
-      console.log(token);
-      navigate("/");
-    }
-  }, [navigate]);
-
-  const handleLogin = () => {
-    setUsername(username);
-    setPassword(password);
-
-    axios
-      .post("/user/login", {
-        account: username,
-        password: password,
-      })
-      .then((response) => {
-        sessionStorage.setItem("token", response.data.token);
-        console.log(sessionStorage.getItem("token"));
-        if (response.data.role_id === 1 || response.data.role_id === 10) {
-          console.log("role_id" + response.data.role_id);
-          const token = sessionStorage.getItem("token");
-          const url = `http://localhost:5000/login?token=${token}`;
-          window.location.href = url;
-        } else {
-          toast.success("Đăng nhập thành công", {
-            position: "bottom-right",
-            autoClose: 2000,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "colored",
-          });
-          const redirectInterval = setInterval(() => {
-            clearInterval(redirectInterval);
-            navigate("/");
-          }, 2000);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        console.log("Đăng nhập thất bại");
-        toast.error(error.response.data.detail, {
-          position: "bottom-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-      });
+  const handleEmailSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedEmail(true);
+    setInputEmail(true);
+    setInputCode(true);
+    setSubmitButtonType("code");
   };
 
+  const handleCodeSubmit = (e) => {
+    e.preventDefault();
+    setInputCode(false);
+    setInputPassword(true);
+    setSubmitButtonType("password");
+  };
+
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault();
+    // Submit password to server
+    console.log("Submitted password:", resetCode);
+  };
   return (
     <section className="vh-100">
-      <ToastContainer />
       <div className="container-fluid h-custom">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-md-9 col-lg-6 col-xl-5">
@@ -85,56 +50,77 @@ function Login() {
               {/* Email input */}
               <div className="form-outline mb-4">
                 <label className="form-label" htmlFor="form3Example3">
-                  Tên đăng nhập
+                  Nhập email
                 </label>
                 <input
-                  value={username}
+                  value={email}
                   type="email"
                   id="form3Example3"
                   className="form-control form-control-lg"
                   placeholder="Enter a valid email address"
-                  onChange={(e) => setUsername(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
 
-              {/* Password input */}
-              <div className="form-outline mb-3">
-                <label className="form-label" htmlFor="form3Example4">
-                  Password
-                </label>
-                <input
-                  value={password}
-                  type="password"
-                  id="form3Example4"
-                  className="form-control form-control-lg"
-                  placeholder="Enter password"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <div className="d-flex justify-content-between align-items-center">
-                {/* Checkbox */}
-                <div className="form-check mb-0"></div>
-                <a href="#!" className="text-body">
-                  Forgot password?
-                </a>
-              </div>
-
+              {inputEmail && (
+                <>
+                  {inputCode && (
+                    <div className="form-outline mb-3">
+                      <label className="form-label" htmlFor="form3Example4">
+                        Nhập mã
+                      </label>
+                      <input
+                        type="password"
+                        id="form3Example4"
+                        className="form-control form-control-lg"
+                        placeholder="Enter password"
+                        onChange={(e) => setResetCode(e.target.value)}
+                      />
+                    </div>
+                  )}
+                </>
+              )}
+              {inputPassword && (
+                <>
+                  <div className="form-outline mb-3">
+                    <label className="form-label" htmlFor="form3Example4">
+                      Mật khẩu mới
+                    </label>
+                    <input
+                      type="password"
+                      id="form3Example4"
+                      className="form-control form-control-lg"
+                      placeholder="Enter password"
+                    />
+                  </div>
+                  <div className="form-outline mb-3">
+                    <label className="form-label" htmlFor="form3Example4">
+                      Nhâp lại mật khẩu mới
+                    </label>
+                    <input
+                      type="password"
+                      id="form3Example4"
+                      className="form-control form-control-lg"
+                      placeholder="Enter password"
+                    />
+                  </div>
+                </>
+              )}
               <div className="text-center text-lg-start mt-4 pt-2">
                 <button
                   type="button"
                   className="btn btn-primary btn-lg"
                   style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
-                  onClick={handleLogin}
+                  onClick={
+                    submitButtonType === "email"
+                      ? handleEmailSubmit
+                      : submitButtonType === "code"
+                      ? handleCodeSubmit
+                      : handlePasswordSubmit
+                  }
                 >
-                  Đăng nhập
+                  Xác nhận
                 </button>
-                <p className="small fw-bold mt-2 pt-1 mb-0">
-                  Không có tài khoản?{" "}
-                  <a href="#!" className="link-danger">
-                    Đăng ký
-                  </a>
-                </p>
               </div>
             </form>
           </div>
