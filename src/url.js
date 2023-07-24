b50f3faa01c83214c7148009125e3c78014d8808
NVH <nguyenvanhung08012002@gmail.com>
2023-07-24 21:47:28 +0700

    update

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-// export const ADMIN_URL = 'http://45.124.95.80:80';
-// export const CLIENT_URL = 'http://45.124.95.80:443';
+export const ADMIN_URL = "http://dhsgundam.online:80";
+export const CLIENT_URL = "http://dhsgundam.online:433";
 
-export const ADMIN_URL = "http://localhost:5000";
-export const CLIENT_URL = "http://localhost:3000";
+// export const ADMIN_URL = "http://localhost:5000";
+// export const CLIENT_URL = "http://localhost:3000";
